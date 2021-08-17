f079c85519619418f2e81819b309b36834155cf5
Dominic <you@example.com>
2021-08-17 18:45:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Logical Connectives in the Statute Book
+
+### Installation
+
+ 1. Install the latest version of [Python](https://www.python.org/downloads/). The application was originally built with Python 3.7.9 on Windows 10.
+ 3. Create a folder e.g. 'lcituksb' and clone the respository https://github.com/domlove/lcituksb.git inside it
+ 4. Make a virtual envronment inside 'lcituksb': ```python -m venv venv``` and then run ```.\venv\Scripts\activate.bat``` to enter it
+ 5. Install the libraries for the virtual environment using ```pip install -r requirements.txt``` 
+ 6. Run ```python app.py``` 
+
+
+### Dash resources
+ - Dash documentation https://dash.plotly.com/
+ - Videos https://www.youtube.com/results?search_query=dash+plotly
+ - Gallery https://dash-gallery.plotly.host/Portal/ 
---
+++ app.py
@@ -0,0 +1,79 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+import dash
+import dash_html_components as html
+import spacy
+from spacy import displacy 
+import json
+import dash_core_components as dcc
+
+# Initialize the application
+app = dash.Dash(__name__)
+
+DEFAULT_LABEL_COLORS = {"(P ∧ Q)":"lightblue", "(¬ P)":"lightgreen","(P ∨ Q)":"orange","(P ← Q)":"purple","(P ↓ Q)":"light red","(P → Q)":"grey"}
+
+def entname(name):
+    return html.Span(name, style={
+        "font-size": "0.8em",
+        "font-weight": "bold",
+        "line-height": "2",
+        "border-radius": "0.35em",
+        "text-transform": "uppercase",
+        "vertical-align": "middle",
+        "margin-left": "0.5rem",
+    })
+
+
+def entbox(children, color):
+    return html.Mark(children, style={
+        "background": color,
+        "padding": "0.45em 0.6em",
+        "margin": "0 0.25em",
+        "line-height": "2",
+        "border-radius": "0.35em",
+    })
+
+
+def entity(children, name):
+    if type(children) is str:
+        children = [children]
+
+    children.append(entname(name))
+    color = DEFAULT_LABEL_COLORS[name]
+    return entbox(children, color)
+
+
+def render(doc):
+    children = []
+    last_idx = 0
+    for ent in doc.ents:
+        children.append(doc.text[last_idx:ent.start_char])
+        children.append(
+            entity(doc.text[ent.start_char:ent.end_char], ent.label_))
+        last_idx = ent.end_char
+    children.append(doc.text[last_idx:])
+    return children
+
+data = requests.get("https://www.legislation.gov.uk/ukpga/Geo5/8-9/47/enacted/data.html") 
+content = data.content
+
+soup = BeautifulSoup(content, features="html.parser")
+clean_content = soup.get_text()
+text = clean_content
+
+
+#text = "When Sebastian and Thrun started working on self-driving cars or at Google if in 2007, then few people outside of the company took him seriously."
+nlp = spacy.load(r".\model")
+doc = nlp(text)
+print("Entities:", doc.ents)
+#displacy.render(doc, style="ent", options=options)
+
+# define de app
+app.layout = html.Div(
+    children=render(doc)
+)
+
+# Run the app
+if __name__ == "__main__":
+    app.run_server(debug=True)
