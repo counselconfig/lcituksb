8d54b2000b34521fca57d0743e31e6adf3e639be
thoriumninty <38498923+thoriumninty@users.noreply.github.com>
2023-06-24 23:16:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import dash
-import dash_core_components as dcc
-#from dash import dcc
-import dash_html_components as html
-#from dash import html
-import dash_table as dt
-#from dash import dash_table as dt
+#import dash_core_components as dcc
+from dash import dcc
+#import dash_html_components as html
+from dash import html
+#import dash_table as dt
+from dash import dash_table as dt
 from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
 from dash.exceptions import PreventUpdate
